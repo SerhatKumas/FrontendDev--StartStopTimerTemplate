2139d6ec2c7320ed9bba6502215fd28ce2ed55ad
Serhat Kumas <serhat.kumas@hotmail.com>
2022-05-19 12:07:12 +0300

    Main page development is completed

---
+++ public/javascript-files/timer.js
@@ -0,0 +1,24 @@
+
+let timer = document.getElementById("timerScreen");
+
+let time = 0;
+
+function startTimer(){
+
+}
+
+function stopTimer(){
+    
+}
+
+function resetTimer(){
+    
+}
+
+function printTime(){
+
+}
+
+function increaseTime(){
+    
+}
