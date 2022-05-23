a878a9ae885ec06a389c544637dedc9c937c1a8f
Serhat Kumas <serhat.kumas@hotmail.com>
2022-05-24 00:05:08 +0300

    Timer development is completed

--- public/javascript-files/timer.js
+++ public/javascript-files/timer.js
@@ -1,24 +1,83 @@
+let milliseconds = 0;
+let seconds = 0;
+let minute = 0;
+let hour = 0;
+let intervalIncrease = null;
+let intervalPrint = null;
+let isRunning = false;
+let isReset = false;
 
-let timer = document.getElementById("timerScreen");
+function startTimer() {
 
-let time = 0;
+   if (isRunning) {
+      alert("Clock is already running!");
+   } else {
+      intervalIncrease = setInterval(increaseTime, 10.095);
+      intervalPrint = setInterval(printTime, 10);
+      isRunning = true;
+      isReset = false;
+   }
 
-function startTimer(){
 
 }
 
-function stopTimer(){
-    
+function resetTimer() {
+
+   if (isReset) {
+      alert("Clock is already reset!");
+   } else {
+      milliseconds = 0;
+      seconds = 0;
+      minute = 0;
+      hour = 0;
+      clearInterval(intervalIncrease);
+      clearInterval(intervalPrint);
+      printTime();
+      isRunning = false;
+      isReset = true;
+   }
+
 }
 
-function resetTimer(){
-    
+function pauseTimer() {
+
+   if (!isRunning && !isReset) {
+      alert("Clock is already paused!");
+   } else if (!isRunning && isReset) {
+      alert("Clock should be started to be paused!");
+   } else {
+      clearInterval(intervalIncrease);
+      clearInterval(intervalPrint);
+      printTime();
+      isRunning = false;
+   }
+
 }
 
-function printTime(){
-
+function increaseTime() {
+   milliseconds += 1;
 }
 
-function increaseTime(){
-    
+function timeCheck() {
+   if (milliseconds >= 100) {
+      milliseconds = 0;
+      seconds += 1;
+   }
+   if (seconds >= 60) {
+      seconds = 0;
+      minute += 1;
+   }
+   if (minute >= 60) {
+      minute = 0;
+      hour += 1;
+   }
+}
+
+function printTime() {
+   timeCheck();
+
+   document.getElementById("hour").innerHTML = hour.toString();
+   document.getElementById("minute").innerHTML = minute.toString();
+   document.getElementById("seconds").innerHTML = seconds.toString();
+   document.getElementById("milliseconds").innerHTML = milliseconds.toString();
 }
